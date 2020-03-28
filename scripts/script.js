baea66284e7bd02e9cf1aa4a94ebf93ec68997fc
Igor Boitsev <igor.boitsev@gmail.com>
2020-03-28 21:38:04 +0300

    Выполнил усложненное задание

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,22 @@
-alert('Привет!');
+// №1
+let num = 266219;
+let str = String(num);     // Перевод числа в строку
+let figuresOfNumer = [];
+let comp = 1;
+
+// №2
+for (i=0; i < String(num).length; i++) {
+  figuresOfNumer[i] = str[i];     // Задание массива, состоящего
+  comp *= str[i];          // цифр заданного числа
+}
+//console.log(figuresOfNumer);
+//console.log(comp);
+
+// №3
+let compInThirdPower = comp ** 3;
+//console.log(compInThirdPower);
+
+// №4
+alert('Первые две цифры полученного вот того здоровенного числа: '
+      + String(compInThirdPower)[0] + ' и '
+      + String(compInThirdPower)[1]);
