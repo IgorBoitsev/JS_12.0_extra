ddce4634975fb2914cc3088ef7a4396ef2667ab4
Igor Boitsev <igor.boitsev@gmail.com>
2020-04-01 22:36:15 +0300

    Должно все работать

--- scripts/script.js
+++ scripts/script.js
@@ -6,10 +6,10 @@ let arr = [
             '24568',
             '45687963',
             '1539',
-            '4865196',
-            '468435',
+            '9865196',
+            '368435',
             '861651',
-            '321684'
+            '421684'
           ];
 
 let startWithTwoOrFour = function(array) {
@@ -20,11 +20,25 @@ let startWithTwoOrFour = function(array) {
   }
   return arraySeparate;
 }
-
+console.log('Первое задание\n------***------');
 console.log(startWithTwoOrFour(arr));
 
 // -------------------------------------
 // №2
 let primeNumbers = function(n) {
   
+  for (let i = 2; i <= n; i++) {
+    // Счетчик для подсчета делителей
+    let divCount = 0;
+    // В цикле проверяем, сколько делителей есть у числа
+    for (let j = 2; j <= i; j++) {
+      if (i % j == 0)
+        divCount += 1; 
+    }
+    // Выводим число, если нацело получилось поделить только на само себя
+    if (divCount == 1) console.log(i + '\nДелители числа: 1 и ' + i);
+  }
 }
+
+console.log('Второе задание\n------***------');
+primeNumbers(100);
