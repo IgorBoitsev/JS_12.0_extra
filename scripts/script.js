6bf6702f0a2aa5803362db155012748df96e73f3
Igor Boitsev <igor.boitsev@gmail.com>
2020-04-04 01:08:02 +0300

    Почему сегодняшний день показывает воскресенье?

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,6 @@
 'use strict';
 
-// -------------------------------------
-// №1
-let arr = [
+let week  = [
             'Понедельник',
             'Вторник',
             'Среда',
@@ -12,4 +10,29 @@ let arr = [
             'Воскресенье'
           ];
 
-          
+let getCurrentDay = function() {
+  let day = new Date();
+  return day.getDay();
+}
+
+let currentDay = getCurrentDay();
+console.log(currentDay);
+console.log(week);
+
+for (let key in week) {
+
+  if ((week[key] == 'Суббота') || (week[key] == 'Воскресенье')) {
+    if (key == currentDay) {
+      document.write('<h2><i><b>' + week[key] + '</b></i></h2>');
+    } else {
+        document.write('<p><i>' + week[key] + '</i></p>');
+      }
+  } else if (key == currentDay) {
+      document.write('<h2><b>' + week[key] + '</b></h2>');
+    } else {
+        document.write('<p>' + week[key] + '</p>');
+      }
+}
+
+
+
