be2cd7d9383599be3cc4bfcda7c8553404d0187c
Igor Boitsev <igor.boitsev@gmail.com>
2020-04-08 00:18:09 +0300

    Осталось сделать вывод через каждую секунду

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,11 @@
 'use strict';
-
-let week  = [
+let seconds,
+    minutes,
+    hours,
+    dayOfWeek,
+    dayNumber,
+    monthNumber,
+    week = [
             'Воскресенье',
             'Понедельник',
             'Вторник',
@@ -8,31 +13,132 @@ let week  = [
             'Четверг',
             'Пятница',
             'Суббота'
-          ];
+          ],
+    month = [
+            'января',
+            'февраля',
+            'марта',
+            'апреля',
+            'мая',
+            'июня',
+            'июля',
+            'августа',
+            'сентября',
+            'октября',
+            'ноября',
+            'декабря',
+          ],
+    year;
+let currentDay = new Date();
+seconds = currentDay.getSeconds();
+minutes = currentDay.getMinutes();
+hours = currentDay.getHours();
 
-let getCurrentDay = function() {
-  let day = new Date();
-  return day.getDay();
+dayNumber = currentDay.getDate() + 1;
+monthNumber = currentDay.getMonth();
+dayOfWeek = week[currentDay.getDay()];
+
+let displayDate = function(d) {
+  // Создание строки с датой
+  let todaysDate = 'Сегодня ' + week[d.getDay()] + ', ' + d.getDate() +
+  ' ' + month[d.getMonth()] + ' ' + d.getFullYear() + ' года, ';
+  
+  return todaysDate;
 }
 
-let currentDay = getCurrentDay();
-console.log(currentDay);
-console.log(week);
+let displayTime = function(h, m, s) {
+  let time = '';
 
-for (let key in week) {
+  // Изменение окончания в зависимости от текущего часа
+  if (h == 1 || h == 21) {
+    time = h + ' час ';
+  } else if ((h < 5 && h > 1) || h > 21) {
+      time = h + ' часа ';
+    } else {
+        time = h + ' часов ';
+      }
 
-  if ((week[key] == 'Суббота') || (week[key] == 'Воскресенье')) {
-    if (key == currentDay) {
-      document.write('<h2><i><b>' + week[key] + '</b></i></h2>');
+  // Изменение окончания в зависимости от текущих минут
+  if (m == 1 ||
+      m == 21 ||
+      m == 31 ||
+      m == 41 ||
+      m == 51) {
+    time = time + m +  ' минута ';
+  } else if ( (m > 1 && m < 5) ||
+              (m > 21 && m < 25) ||
+              (m > 31 && m < 35) ||
+              (m > 41 && m < 45) ||
+              (m > 51 && m < 55)) {
+      time = time + m +  ' минуты ';
     } else {
-        document.write('<p><i>' + week[key] + '</i></p>');
+        time = time + m +  ' минут ';
       }
-  } else if (key == currentDay) {
-      document.write('<h2><b>' + week[key] + '</b></h2>');
+
+  // Изменение окончания в зависимости от текущих секунд
+  if (s == 1 ||
+      s == 21 ||
+      s == 31 ||
+      s == 41 ||
+      s == 51) {
+    time = time + s +  ' секунда ';
+  } else if ( (s > 1 && s < 5) ||
+              (s > 21 && s < 25) ||
+              (s > 31 && s < 35) ||
+              (s > 41 && s < 45) ||
+              (s > 51 && s < 55)) {
+      time = time + s +  ' секунды ';
     } else {
-        document.write('<p>' + week[key] + '</p>');
+        time = time + s +  ' секунд ';
       }
+
+  return time;
 }
 
+let displayDigitalDate = function(d) {
+  let digitalDate = '';
+  
+  // Проверка номера дня месяца для простановки 0 впереди
+  if (d.getDate() < 9) {
+    digitalDate = digitalDate + '0' + d.getDate();
+  }  else {
+    digitalDate = digitalDate + d.getDate();
+    }
 
+  // Проверка номера месяца для простановки 0 впереди
+  if (d.getMonth() < 8) {
+    digitalDate = digitalDate + '.0' + (d.getMonth() + 1) + '.' + d.getFullYear();
+  } else {
+     digitalDate = digitalDate + '.' + (d.getMonth() + 1) + '.' + d.getFullYear();
+    }
 
+  // Проверка часов на наличие 0 впереди
+  if (hours < 9) {
+    digitalDate = digitalDate + ' - ' + '0' + hours;
+  } else {
+      digitalDate = digitalDate + ' - ' + hours;
+    }
+
+  // Проверка минут на наличие 0 впереди
+  if (minutes < 9) {
+    digitalDate = digitalDate + ':0' + minutes;
+  } else {
+      digitalDate = digitalDate + ':' + minutes;
+    }
+
+  // Проверка минут на наличие 0 впереди
+  if (seconds < 9) {
+    digitalDate = digitalDate + ':0' + seconds;
+  } else {
+      digitalDate = digitalDate + ':' + seconds;
+    }    
+    
+  return digitalDate;
+}
+
+document.write(displayDate(currentDay) + displayTime(hours, minutes, seconds));
+document.write('<br>');
+document.write(displayDigitalDate(currentDay));
+
+console.log(displayDate(currentDay) + displayTime(hours, minutes, seconds));
+console.log(displayDigitalDate(currentDay));
