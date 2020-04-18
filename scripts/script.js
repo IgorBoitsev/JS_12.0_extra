d7bba6b0f8e381cca66a16079b597af35e853a24
Igor Boitsev <igor.boitsev@gmail.com>
2020-04-18 23:53:26 +0300

    first

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+function DomElement(selector, height, width, bg, fontSize) {
+  this.selector = selector;
+  this.height = height;
+  this.width = width;
+  this.bg = bg;
+  this.fontSize = fontSize;
+};
+
+let newDomElement = new DomElement('.block', 100, 100, '#fff000', 16);
+console.log(newDomElement);
