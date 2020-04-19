198171d0d58bb3e74d939e73f4436d4b9259814b
Igor Boitsev <igor.boitsev@gmail.com>
2020-04-19 13:50:43 +0300

    div & p

--- scripts/script.js
+++ scripts/script.js
@@ -31,73 +31,73 @@ DomElement.prototype.createElement = function() {
 
   if (this.selector[0] == '#') {
     // Создание элементов
-    let div = document.createElement('div');
+    let p = document.createElement('p');
     let str = document.createElement('span');
     // Добавление id блоку 
-    div.id = this.selector.substr(1);
+    p.id = this.selector.substr(1);
     // Добавление стилей
-    div.style.cssText =  'height: ' + this.height + 'px;' +
+    p.style.cssText =  'height: ' + this.height + 'px;' +
                           'width: ' + this.width + 'px;' +
                           'background: ' + this.bg + ';';
     str.style.cssText = 'font-size: ' + this.fontSize + 'px;';
     // Вставка текста в строку
     str.innerHTML = this.text;
     // Вставка строки в блок
-    div.append(str);
+    p.append(str);
     //Вставка блока в документ
-    document.body.prepend(div);
+    document.body.prepend(p);
   };
 };
 
 // ============================================================================
-// let newDomElement = new DomElement('.block', 100, 100, '#fff000', 'Привет?', 21);
-// let newDomElement2 = new DomElement('#best', 200, 150, '#000fff', 'Пока!', 22);
-// let newDomElement3 = new DomElement('#best', 140, 110, '#714ffd', 'Пока!', 20);
+let newDomElement = new DomElement('.block', 100, 100, '#fff000', 'Привет?', 21);
+let newDomElement2 = new DomElement('#best', 200, 150, '#000fff', 'Пока!', 22);
+let newDomElement3 = new DomElement('#best', 140, 110, '#714ffd', 'Пока!', 20);
 
-// newDomElement.createElement();
-// newDomElement2.createElement();
-// newDomElement3.createElement();
+newDomElement.createElement();
+newDomElement2.createElement();
+newDomElement3.createElement();
 // ============================================================================
 
-// Обнуление стилей в head для body
-document.body.style.cssText = 'display: block; justify-content: unset';
+// // Обнуление стилей в head для body
+// document.body.style.cssText = 'display: block; justify-content: unset';
 
-let newDomElement = new DomElement('.square', 100, 100, '#ff00ff', 'Погнали!', 22);
-newDomElement.createElement();
+// let newDomElement = new DomElement('.square', 100, 100, '#ff00ff', 'Погнали!', 22);
+// newDomElement.createElement();
 
-// // Добавление стилей блоку
-let square = document.querySelector('.square');
-square.style.cssText = square.getAttribute('style') + ' position: absolute; left: 0px; top: 0px';
+// // // Добавление стилей блоку
+// let square = document.querySelector('.square');
+// square.style.cssText = square.getAttribute('style') + ' position: absolute; left: 0px; top: 0px';
 
-DomElement.prototype.eventsListeners = function() {
-  document.addEventListener('keydown', function(event) {
+// DomElement.prototype.eventsListeners = function() {
+//   document.addEventListener('keydown', function(event) {
     
-    let div = document.querySelector('div'),
-        pxRight = +getComputedStyle(div).right.split('px')[0],
-        pxLeft = +getComputedStyle(div).left.split('px')[0],
-        pxUp = +getComputedStyle(div).top.split('px')[0],
-        pxDown = +getComputedStyle(div).bottom.split('px')[0];
+//     let div = document.querySelector('div'),
+//         pxRight = +getComputedStyle(div).right.split('px')[0],
+//         pxLeft = +getComputedStyle(div).left.split('px')[0],
+//         pxUp = +getComputedStyle(div).top.split('px')[0],
+//         pxDown = +getComputedStyle(div).bottom.split('px')[0];
     
-    if (event.key == 'ArrowRight' && pxRight > 0) {
-      let px = +getComputedStyle(div).left.split('px')[0] + 10;
-      div.style.left = px + 'px';
-    };
+//     if (event.key == 'ArrowRight' && pxRight > 0) {
+//       let px = +getComputedStyle(div).left.split('px')[0] + 10;
+//       div.style.left = px + 'px';
+//     };
 
-    if (event.key == 'ArrowLeft' && pxLeft > 0) {
-      let px = +getComputedStyle(div).left.split('px')[0] - 10;
-      div.style.left = px + 'px';
-    };
+//     if (event.key == 'ArrowLeft' && pxLeft > 0) {
+//       let px = +getComputedStyle(div).left.split('px')[0] - 10;
+//       div.style.left = px + 'px';
+//     };
 
-    if (event.key == 'ArrowUp' && pxUp > 0) {
-      let px = +getComputedStyle(div).top.split('px')[0] - 10;
-      div.style.top = px + 'px';
-    }
+//     if (event.key == 'ArrowUp' && pxUp > 0) {
+//       let px = +getComputedStyle(div).top.split('px')[0] - 10;
+//       div.style.top = px + 'px';
+//     }
 
-    if (event.key == 'ArrowDown' && pxDown > 0) {
-      let px = +getComputedStyle(div).top.split('px')[0] + 10;
-      div.style.top = px + 'px';
-    }
-  });
-};
+//     if (event.key == 'ArrowDown' && pxDown > 0) {
+//       let px = +getComputedStyle(div).top.split('px')[0] + 10;
+//       div.style.top = px + 'px';
+//     }
+//   });
+// };
 
-document.addEventListener('DOMContentLoaded', newDomElement.eventsListeners());
+// document.addEventListener('DOMContentLoaded', newDomElement.eventsListeners());
