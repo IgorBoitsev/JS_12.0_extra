c8326600bb984c62735c05beba79165987d0677b
Igor Boitsev <igor.boitsev@gmail.com>
2020-05-06 11:16:22 +0300

    promise get

--- scripts/script.js
+++ scripts/script.js
@@ -2,27 +2,30 @@ document.addEventListener('DOMContentLoaded', () => {
     'use strict';
 
     const select = document.getElementById('cars'),
-        output = document.getElementById('output');
+          output = document.getElementById('output');
 
-    select.addEventListener('change', () => {
-        const request = new XMLHttpRequest();
-        request.open('GET', './cars.json');
-        request.setRequestHeader('Content-type', 'application/json');
-        request.send();
-        request.addEventListener('readystatechange', () => {
-            if (request.readyState === 4 && request.status === 200) {
-                const data = JSON.parse(request.responseText);
-                data.cars.forEach(item => {
-                    if (item.brand === select.value) {
-                        const {brand, model, price} = item;
-                        output.innerHTML = `Тачка ${brand} ${model} <br>
-                        Цена: ${price}$`;
-                    }
-                });
-            } else {
-                output.innerHTML = 'Произошла ошибка';
-            }
+    const outputMessage = (carData) => {
+        return new Promise(() => {
+            const request = new XMLHttpRequest();
+            request.open('GET', './scripts/cars.json');
+            request.setRequestHeader('Content-type', 'application/json');
+            request.send();
+            request.addEventListener('readystatechange', () => {
+                if (request.readyState === 4 && request.status === 200) {
+                    const data = JSON.parse(request.responseText);
+                    data.cars.forEach(item => {
+                        if (item.brand === select.value) {
+                            const {brand, model, price} = item;
+                            output.innerHTML = `Тачка: ${brand} ${model} <br>
+                            Цена: ${price}$`;
+                        }
+                    });
+                } else {
+                    output.innerHTML = 'Произошла ошибка';
+                }
+            });
         });
-    });
-
+    }
+    
+    select.addEventListener('change', outputMessage);
 });
